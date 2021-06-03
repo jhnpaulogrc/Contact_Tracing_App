532581429277563ed5e9d6ee3eda7302a594d9f8
jhnpaulogrc <johnpaulogarcia03@gmail.com>
2021-06-03 20:49:44 +0800

    Code Redesign

---
+++ Contact Tracing Application/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Contact_Tracing_Application
+{
+    partial class formSubmit
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnSubmit = new System.Windows.Forms.Button();
+            this.bttnView = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtContactNumber = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttnSubmit
+            // 
+            this.bttnSubmit.BackColor = System.Drawing.Color.RoyalBlue;
+            this.bttnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttnSubmit.ForeColor = System.Drawing.Color.White;
+            this.bttnSubmit.Location = new System.Drawing.Point(100, 232);
+            this.bttnSubmit.Name = "bttnSubmit";
+            this.bttnSubmit.Size = new System.Drawing.Size(109, 23);
+            this.bttnSubmit.TabIndex = 0;
+            this.bttnSubmit.Text = "Submit Response";
+            this.bttnSubmit.UseVisualStyleBackColor = false;
+            this.bttnSubmit.Click += new System.EventHandler(this.bttnSubmit_Click);
+            // 
+            // bttnView
+            // 
+            this.bttnView.BackColor = System.Drawing.Color.RoyalBlue;
+            this.bttnView.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttnView.ForeColor = System.Drawing.Color.White;
+            this.bttnView.Location = new System.Drawing.Point(215, 232);
+            this.bttnView.Name = "bttnView";
+            this.bttnView.Size = new System.Drawing.Size(109, 23);
+            this.bttnView.TabIndex = 1;
+            this.bttnView.Text = "View Response";
+            this.bttnView.UseVisualStyleBackColor = false;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(62, 72);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(285, 72);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(39, 23);
+            this.txtAge.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Age:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(76, 116);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(248, 23);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Contact Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email Address:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(123, 178);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(201, 23);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(328, 37);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "CONTACT TRACING FORM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(21, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 11);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Kindly fill up the needed information.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(62, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 11);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "(Surname)";
+            // 
+            // txtContactNumber
+            // 
+            this.txtContactNumber.Location = new System.Drawing.Point(123, 149);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(201, 23);
+            this.txtContactNumber.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(186, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 11);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "(Middle name)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(123, 98);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(46, 11);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "(First name)";
+            // 
+            // formSubmit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(346, 276);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtContactNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.bttnView);
+            this.Controls.Add(this.bttnSubmit);
+            this.Name = "formSubmit";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnSubmit;
+        private System.Windows.Forms.Button bttnView;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtContactNumber;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Contact Tracing Application/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Tracing_Application
+{
+    public partial class formSubmit : Form
+    {
+        public formSubmit()
+        {
+            InitializeComponent();
+        }
+        private void bttnSubmit_Click(object sender, EventArgs e)
+        {
+                StreamWriter outputFile;
+
+                String createDate = DateTime.Now.ToLongDateString();
+
+                outputFile = File.AppendText(createDate);
+                outputFile.WriteLine("Name:" + txtName.Text);
+                outputFile.WriteLine("Age:" + txtAge.Text);
+                outputFile.WriteLine("Address:" + txtAddress.Text);
+                outputFile.WriteLine("Contact Number:" + txtContactNumber.Text);
+                outputFile.WriteLine("Email Address:" + txtEmail.Text);
+                outputFile.WriteLine();
+                outputFile.WriteLine();
+                outputFile.Close();
+
+                MessageBox.Show("Response Submitted!");
+
+                txtName.Text = "";
+                txtAge.Text = "";
+                txtAddress.Text = "";
+                txtContactNumber.Text = "";
+                txtEmail.Text = "";
+            
+        }
+    }
+}
